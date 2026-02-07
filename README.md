479570ff92e260a6ca8f26f8a9470e3d364b7a37
C0l0red <blaizepashcal@gmail.com>
2026-02-07 16:39:27 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,9 @@
+# Simple FS
+This is a simple file system built in Rust.  
+It is for educational purposes, and is my first attempt at
+low-level programming.
+
+## Features
+- Superblock serialization and deserialization
+- Bitmap serialization and deserialization
+- Basic file system operations
---
+++ src/main.rs
@@ -0,0 +1,445 @@
+extern crate core;
+
+use std::fs::File;
+use std::io;
+use std::io::{Read, Seek, SeekFrom, Write};
+use std::ops::{Deref, DerefMut};
+use std::path::{Path, PathBuf};
+
+fn main() {
+    println!("Hello, world!");
+}
+
+const MAGIC_NUMBER: u32 = 0x4D594653; // "MYFS" in ASCII
+const INODE_COUNT: u32 = 8;
+const INODE_SIZE: u32 = 9;
+const BLOCK_SIZE: u32 = 512;
+const TOTAL_BLOCKS: u32 = 16;
+
+#[derive(Debug)]
+enum Error {
+    Validation(String),
+    IO(String),
+}
+
+impl From<io::Error> for Error {
+    fn from(err: io::Error) -> Self {
+        Error::IO(err.to_string())
+    }
+}
+
+trait Disk {
+    fn mount(path: &Path, size: u64, block_index: u64, magic_number: u64) -> Result<Self, Error>
+    where
+        Self: Sized;
+    fn format_disk(file: File, block_index: u32, magic_number: u32) -> Result<(), Error>;
+}
+
+trait BlockDevice {
+    fn read_block(&mut self, block_index: u32, buffer: &mut [u8]) -> Result<(), Error>;
+    fn write_block(&mut self, block_index: u32, data: &[u8]) -> Result<(), Error>;
+    fn block_size(&self) -> u32;
+    fn total_blocks(&self) -> u32;
+}
+
+struct ImgFileDisk {
+    file: File,
+    block_size: u32,
+    total_blocks: u32,
+}
+
+pub struct MyFS<D: BlockDevice> {
+    device: D,
+    superblock: Superblock,
+    inode_bitmap: Bitmap,
+    block_bitmap: Bitmap,
+}
+
+struct Superblock {
+    magic_number: u32,
+    version: u32,
+    block_size: u32,
+    total_blocks: u32,
+    inode_count: u32,
+    inode_size: u32,
+    inode_bitmap_start: u32,
+    data_bitmap_start: u32,
+    inode_table_start: u32,
+    data_block_start: u32,
+}
+
+struct Bitmap(Vec<u8>);
+
+#[derive(Copy, Clone)]
+struct Inode {
+    kind: InodeKind,
+    size: u32,
+    direct_block: u32,
+}
+
+#[repr(u8)]
+#[derive(Copy, Clone)]
+enum InodeKind {
+    File = 0u8,
+    Directory = 1u8,
+}
+
+struct DirectoryEntry {
+    inode: u32,
+    name_len: u8,
+    name: [u8; 256],
+}
+
+impl Deref for Bitmap {
+    type Target = [u8];
+    fn deref(&self) -> &Self::Target {
+        &self.0
+    }
+}
+
+impl DerefMut for Bitmap {
+    fn deref_mut(&mut self) -> &mut Self::Target {
+        &mut self.0
+    }
+}
+
+impl Bitmap {
+    fn new() -> Self {
+        Self(vec![0u8; BLOCK_SIZE as usize])
+    }
+
+    fn create_and_occupy_first_n_bits(occupied_offset: usize) -> Self {
+        let mut bitmap = Self::new();
+        let full_bytes = occupied_offset / 8;
+        let remaining_bits = occupied_offset % 8;
+
+        // set complete bytes
+        for i in 0..full_bytes {
+            bitmap[i] = 0xFF;
+        }
+
+        // set remaining bits in the next byte
+        if remaining_bits > 0 {
+            bitmap[full_bytes] |= (1 << remaining_bits) - 1;
+        }
+        bitmap
+    }
+
+    fn set_bit(&mut self, index: usize) {
+        let byte = index / 8;
+        let bit = index % 8;
+        self[byte] |= 1 << bit;
+    }
+
+    fn clear_bit(&mut self, index: usize) {
+        let byte = index / 8;
+        let bit = index % 8;
+        self[byte] &= !(1 << bit);
+    }
+
+    fn is_bit_set(&self, index: usize) -> bool {
+        let byte = index / 8;
+        let bit = index % 8;
+        self[byte] & (1 << bit) != 0
+    }
+}
+
+impl TryFrom<u8> for InodeKind {
+    type Error = Error;
+
+    fn try_from(value: u8) -> Result<Self, Self::Error> {
+        match value {
+            0 => Ok(Self::File),
+            1 => Ok(Self::Directory),
+            _ => Err(Error::Validation("Invalid inode kind".to_string())),
+        }
+    }
+}
+
+impl Inode {
+    fn new(kind: InodeKind, size: u32, direct_block: u32) -> Self {
+        Self {
+            kind,
+            size,
+            direct_block,
+        }
+    }
+}
+
+trait BytesSerializable {
+    fn bytes_to_u32(bytes: &[u8]) -> Result<u32, Error> {
+        Ok(u32::from_le_bytes(bytes.try_into().map_err(|_| {
+            Error::Validation("Bytes to u32 conversion failed".to_string())
+        })?))
+    }
+
+    fn to_bytes(&self) -> Vec<u8>;
+    fn try_from_bytes(bytes: &[u8]) -> Result<Self, Error>
+    where
+        Self: Sized;
+}
+
+impl BytesSerializable for Inode {
+    fn to_bytes(&self) -> Vec<u8> {
+        let mut buffer: Vec<u8> = vec![0u8; INODE_SIZE as usize];
+
+        buffer[0] = self.kind as u8;
+        buffer[1..5].copy_from_slice(&self.size.to_le_bytes());
+        buffer[5..9].copy_from_slice(&self.direct_block.to_le_bytes());
+
+        buffer
+    }
+
+    fn try_from_bytes(buffer: &[u8]) -> Result<Self, Error> {
+        if buffer.len() < 9 {
+            return Err(Error::Validation(
+                "Buffer is too short to contain an Inode".to_string(),
+            ));
+        }
+
+        Ok(Inode {
+            kind: InodeKind::try_from(buffer[0])?,
+            size: Self::bytes_to_u32(&buffer[1..5])?,
+            direct_block: Self::bytes_to_u32(&buffer[5..9])?,
+        })
+    }
+}
+
+impl BytesSerializable for Superblock {
+    fn to_bytes(&self) -> Vec<u8> {
+        let mut buffer: Vec<u8> = vec![0u8; BLOCK_SIZE as usize];
+
+        buffer[0..4].copy_from_slice(&self.magic_number.to_le_bytes());
+        buffer[4..8].copy_from_slice(&self.version.to_le_bytes());
+        buffer[8..12].copy_from_slice(&self.block_size.to_le_bytes());
+        buffer[12..16].copy_from_slice(&self.total_blocks.to_le_bytes());
+        buffer[16..20].copy_from_slice(&self.inode_count.to_le_bytes());
+        buffer[20..24].copy_from_slice(&self.inode_size.to_le_bytes());
+        buffer[24..28].copy_from_slice(&self.inode_bitmap_start.to_le_bytes());
+        buffer[28..32].copy_from_slice(&self.data_bitmap_start.to_le_bytes());
+        buffer[32..36].copy_from_slice(&self.inode_table_start.to_le_bytes());
+        buffer[36..40].copy_from_slice(&self.data_block_start.to_le_bytes());
+
+        buffer
+    }
+
+    fn try_from_bytes(buffer: &[u8]) -> Result<Self, Error> {
+        if buffer.len() < BLOCK_SIZE as usize {
+            return Err(Error::Validation(
+                "Buffer is too short to contain a Superblock".to_string(),
+            ));
+        }
+
+        Ok(Superblock {
+            magic_number: Self::bytes_to_u32(&buffer[0..4])?,
+            version: Self::bytes_to_u32(&buffer[4..8])?,
+            block_size: Self::bytes_to_u32(&buffer[8..12])?,
+            total_blocks: Self::bytes_to_u32(&buffer[12..16])?,
+            inode_count: Self::bytes_to_u32(&buffer[16..20])?,
+            inode_size: Self::bytes_to_u32(&buffer[20..24])?,
+            inode_bitmap_start: Self::bytes_to_u32(&buffer[24..28])?,
+            data_bitmap_start: Self::bytes_to_u32(&buffer[28..32])?,
+            inode_table_start: Self::bytes_to_u32(&buffer[32..36])?,
+            data_block_start: Self::bytes_to_u32(&buffer[36..40])?,
+        })
+    }
+}
+
+impl ImgFileDisk {
+    fn open(path: &Path) -> Result<Self, Error> {
+        if !path.is_file() {
+            return Err(Error::IO("Disk file not found".to_string()));
+        }
+        if !path
+            .extension()
+            .is_some_and(|ext| ext.to_str() == Some("img"))
+        {
+            return Err(Error::Validation(
+                "Disk file is not an img file".to_string(),
+            ));
+        }
+
+        Ok(ImgFileDisk {
+            file: File::options()
+                .read(true)
+                .write(true)
+                .open(path)
+                .map_err(|_| Error::IO("Could not open disk file".to_string()))?,
+            block_size: BLOCK_SIZE,
+            total_blocks: TOTAL_BLOCKS,
+        })
+    }
+}
+
+impl BlockDevice for ImgFileDisk {
+    fn read_block(&mut self, block_index: u32, buffer: &mut [u8]) -> Result<(), Error> {
+        self.file
+            .seek(SeekFrom::Start((block_index * self.block_size) as u64))
+            .map_err(|_| {
+                Error::IO("Could not seek while reading block from img file".to_string())
+            })?;
+        self.file
+            .read_exact(buffer)
+            .map_err(|_| Error::IO("Could not read block from img file".to_string()))?;
+
+        Ok(())
+    }
+
+    fn write_block(&mut self, block_index: u32, buffer: &[u8]) -> Result<(), Error> {
+        self.file
+            .seek(SeekFrom::Start((block_index * self.block_size) as u64))
+            .map_err(|_| Error::IO("Could not seek while writing block to img file".to_string()))?;
+        self.file
+            .write_all(buffer)
+            .map_err(|_| Error::IO("Could not write block to img file".to_string()))?;
+
+        Ok(())
+    }
+
+    fn block_size(&self) -> u32 {
+        self.block_size
+    }
+
+    fn total_blocks(&self) -> u32 {
+        self.total_blocks
+    }
+}
+
+impl<D: BlockDevice> MyFS<D> {
+    pub fn mount(mut device: D) -> Result<Self, Error> {
+        // read superblock
+        // validate magic
+        // load bitmaps
+        // construct MyFS
+        todo!()
+    }
+
+    pub fn format(mut device: D) -> Result<(), Error> {
+        let mut buffer = vec![0u8; device.block_size() as usize];
+
+        // Write superblock
+        let super_block = Superblock {
+            magic_number: MAGIC_NUMBER,
+            version: 1,
+            block_size: device.block_size(),
+            total_blocks: TOTAL_BLOCKS,
+            inode_count: INODE_COUNT,
+            inode_size: INODE_SIZE,
+            inode_bitmap_start: 1,
+            data_bitmap_start: 2,
+            inode_table_start: 3,
+            data_block_start: 4,
+        };
+        buffer.copy_from_slice(super_block.to_bytes().as_slice());
+        device.write_block(0, buffer.as_slice())?;
+
+        // Write bitmap blocks
+        // Leave 1 bit occupied for the root directory inode
+        let inode_bitmap = Bitmap::create_and_occupy_first_n_bits(1);
+        buffer.copy_from_slice(&inode_bitmap);
+        device.write_block(1, buffer.as_slice())?;
+
+        // Write bitmap blocks
+        // Leave 5 bits occupied; 4 for metadata blocks, 1 for root directory block
+        let block_bitmap = Bitmap::create_and_occupy_first_n_bits(5);
+        buffer.copy_from_slice(&block_bitmap);
+        device.write_block(2, buffer.as_slice())?;
+
+        // Write inode table
+        let root_directory_inode = Inode::new(InodeKind::Directory, 0, 4);
+        buffer.fill(0u8);
+        buffer[0..INODE_SIZE as usize].copy_from_slice(root_directory_inode.to_bytes().as_slice());
+        device.write_block(3, buffer.as_slice())?;
+
+        Ok(())
+    }
+}
+
+// impl ImgFileDisk {
+//     fn validate_img_file(&self, block_size: u32, magic_number: u32) -> Result<(), Error> {
+//         let mut buffer = vec![0u8; block_size as usize];
+//         let mut file = File::open(&self.file)
+//             .map_err(|_| Error::Validation("Could not open disk file".to_string()))?;
+//
+//         let bytes_read = file
+//             .read(buffer.as_mut_slice())
+//             .map_err(|_| Error::Validation("Could not read disk file".to_string()))?;
+//         if bytes_read == 0 {
+//             return Self::format_disk(file, block_size, magic_number);
+//         }
+//         if bytes_read != block_size as usize {
+//             return Err(Error::Validation("File is too small".to_string()));
+//         }
+//
+//         let super_block = Superblock::try_from_bytes(&buffer)
+//             .map_err(|_| Error::Validation("Could not read superblock".to_string()))?;
+//
+//         if super_block.magic_number != magic_number {
+//             return Err(Error::Validation(
+//                 "File is not a valid Disk for this file system".to_string(),
+//             ));
+//         }
+//
+//         Ok(())
+//     }
+// }
+
+#[cfg(test)]
+mod tests {
+    use crate::{Bitmap, BytesSerializable, MAGIC_NUMBER, Superblock, BLOCK_SIZE, TOTAL_BLOCKS, INODE_COUNT, INODE_SIZE};
+
+    #[test]
+    fn serialize_superblock_to_bytes() {
+        let superblock = Superblock {
+            magic_number: MAGIC_NUMBER,
+            version: 1,
+            block_size: 512,
+            total_blocks: 16,
+            inode_count: 8,
+            inode_size: 9,
+            inode_bitmap_start: 1,
+            data_bitmap_start: 2,
+            inode_table_start: 3,
+            data_block_start: 4,
+        };
+
+        let buffer = superblock.to_bytes();
+
+        assert_eq!(buffer.len(), BLOCK_SIZE as usize);
+        assert_eq!(buffer[0..4], MAGIC_NUMBER.to_le_bytes());
+        assert_eq!(buffer[4..8], superblock.version.to_le_bytes());
+        assert_eq!(buffer[8..12], superblock.block_size.to_le_bytes());
+        assert_eq!(buffer[12..16], superblock.total_blocks.to_le_bytes());
+        assert_eq!(buffer[16..20], superblock.inode_count.to_le_bytes());
+        assert_eq!(buffer[20..24], superblock.inode_size.to_le_bytes());
+        assert_eq!(buffer[24..28], superblock.inode_bitmap_start.to_le_bytes());
+        assert_eq!(buffer[28..32], superblock.data_bitmap_start.to_le_bytes());
+        assert_eq!(buffer[32..36], superblock.inode_table_start.to_le_bytes());
+        assert_eq!(buffer[36..40], superblock.data_block_start.to_le_bytes());
+    }
+
+    #[test]
+    fn deserialize_superblock_from_bytes() {
+        let mut buffer: Vec<u8> = vec![83, 70, 89, 77, 1, 0, 0, 0, 0, 2, 0, 0, 16, 0, 0, 0, 8, 0, 0, 0, 9, 0, 0, 0, 1, 0, 0, 0, 2, 0, 0, 0, 3, 0, 0, 0, 4, 0, 0, 0];
+        buffer.resize(BLOCK_SIZE as usize, 0u8);
+        let superblock = Superblock::try_from_bytes(&buffer).unwrap();
+
+        assert_eq!(superblock.magic_number, MAGIC_NUMBER);
+        assert_eq!(superblock.version, 1);
+        assert_eq!(superblock.block_size, BLOCK_SIZE);
+        assert_eq!(superblock.total_blocks, TOTAL_BLOCKS);
+        assert_eq!(superblock.inode_count, INODE_COUNT);
+        assert_eq!(superblock.inode_size, INODE_SIZE);
+        assert_eq!(superblock.inode_bitmap_start, 1);
+        assert_eq!(superblock.data_bitmap_start, 2);
+        assert_eq!(superblock.inode_table_start, 3);
+        assert_eq!(superblock.data_block_start, 4);
+    }
+
+    #[test]
+    fn create_bitmap_and_occupy_first_n_bits() {
+        let bitmap = Bitmap::create_and_occupy_first_n_bits( 4);
+        assert_eq!(bitmap[0], 0b1111);
+        assert_eq!(bitmap[1], 0b0000);
+    }
+}
